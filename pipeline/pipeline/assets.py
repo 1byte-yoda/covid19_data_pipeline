9ea2bc93c2c8b71578b009eaccddc3cdcad13315
1byte-yoda <dmc.markr@gmail.com>
2025-04-27 06:14:03 +0800

    Dagster Pipeline with Github Covid19 Dataset Ingester

---
+++ pipeline/pipeline/assets.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import dlt
+from dagster_dbt import DbtCliResource, dbt_assets
+from dagster import DailyPartitionsDefinition
+from dagster import AssetExecutionContext
+from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets
+from .covid19_github_csse import github_source
+
+from .project import covid19_dbt_project
+
+daily_partitions = DailyPartitionsDefinition(start_date="01-02-2023", fmt="%m-%d-%Y")
+# end_date="01-10-2023",
+# "03-09-2023"
+
+@dlt_assets (
+    dlt_source=github_source(),
+    dlt_pipeline=dlt.pipeline(
+        pipeline_name="github_csse_daily",
+        destination=dlt.destinations.filesystem(),
+        dataset_name="covid19"
+    ),
+    partitions_def=daily_partitions
+)
+def covid19_github_csse_assets(context: AssetExecutionContext, dagster_dlt: DagsterDltResource):
+    partition_key_range = context.partition_key_range
+    start_date = datetime.strptime(partition_key_range.start, "%m-%d-%Y").date()
+
+    if partition_key_range.end:
+        end_date = datetime.strptime(partition_key_range.end, "%m-%d-%Y").date()
+
+    else:
+        end_date = start_date
+
+    yield from dagster_dlt.run(context=context, dlt_source=github_source(start_date=start_date, end_date=end_date))
+
+
+@dbt_assets(manifest=covid19_dbt_project.manifest_path)
+def covid19_dbt_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
---
+++ pipeline/pipeline/covid19_github_csse.py
@@ -0,0 +1,71 @@
+import hashlib
+from datetime import date, timedelta
+from io import BytesIO
+from typing import Optional
+
+import requests
+from urllib.error import HTTPError
+from loguru import logger
+import pandas as pd
+import dlt
+
+
+def _safe_download_covid_df_with_md5(url: str) -> pd.DataFrame:
+    try:
+        print("Downloading CSV from:", url)
+        response = requests.get(url)
+        md5_hash = hashlib.md5(response.content).hexdigest()
+        df = pd.read_csv(BytesIO(response.content))
+        df["file_md5"] = md5_hash
+        return df
+    except HTTPError as e:
+        logger.exception(e)
+        return pd.DataFrame()
+
+
+def _create_hive_partition_fields(df: pd.DataFrame, ingest_date: date) -> pd.DataFrame:
+    logger.debug(f"Hive partition date to create: {ingest_date}")
+    df["load_date"] = ingest_date
+    df["year"] = ingest_date.year
+    df["month"] = ingest_date.month
+    df["day"] = ingest_date.day
+    return df
+
+
+def generate_md5(row: pd.Series):
+    concat_str = f"{row['file_md5']}_{row['row_num']}"
+    return hashlib.md5(concat_str.encode('utf-8')).hexdigest()
+
+
+@dlt.resource(
+    name="covid_cases",
+    table_format="delta",
+    table_name="github_csse_daily",
+    columns={"year": {"partition": True}, "month": {"partition": True}, "day": {"partition": True}},
+    primary_key="id",
+    write_disposition={"disposition": "merge", "strategy": "upsert"},
+)
+def get_github_csse_daily(start_date: date, end_date: date):
+    partition_date = start_date
+    while partition_date <= end_date:
+        query_date = partition_date.strftime("%m-%d-%Y")
+        url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_daily_reports/{query_date}.csv"
+        df = _safe_download_covid_df_with_md5(url=url)
+        df = df.reset_index(names="row_num")
+        df["id"] = df.apply(generate_md5, axis=1)
+        df["source_url"] = url
+        df["before_load_rows"] = df.shape[0]
+        df["before_load_cols"] = str(list(df.columns))
+        df = _create_hive_partition_fields(df=df, ingest_date=partition_date)
+        df["after_load_rows"] = df.shape[0]
+        df["after_load_cols"] = str(list(df.columns))
+        yield df
+        partition_date = partition_date + timedelta(days=1)
+
+
+@dlt.source
+def github_source(start_date: date = None, end_date: Optional[date] = None):
+    if start_date is None:
+        start_date = date.today()
+        end_date = date.today()
+    return get_github_csse_daily(start_date=start_date, end_date=end_date)
