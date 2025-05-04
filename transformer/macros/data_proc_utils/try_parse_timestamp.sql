120cda5d5b62f7af123703b27f763c3918554001
1byte-yoda <dmc.markr@gmail.com>
2025-05-04 20:49:12 +0800

    Cast macro result into timestamp

--- transformer/macros/data_proc_utils/try_parse_timestamp.sql
+++ transformer/macros/data_proc_utils/try_parse_timestamp.sql
@@ -8,7 +8,8 @@ COALESCE(
   TRY_STRPTIME({{ column_name }}, '%-m/%-d/%Y %H:%M'),       -- 1/22/2020 17:00
   TRY_STRPTIME({{ column_name }}, '%Y/%m/%d %H:%M:%S'),      -- 2023/01/06 04:21:02
   TRY_STRPTIME({{ column_name }}, '%m/%d/%Y %H:%M'),         -- 03/08/2020 05:19
-  TRY_STRPTIME({{ column_name }}, '%m/%d/%y %H:%M:%S')       -- 03/08/20 05:19:00
-)
+  TRY_STRPTIME({{ column_name }}, '%m/%d/%y %H:%M:%S'),       -- 03/08/20 05:19:00
+  '1900-01-01'
+)::TIMESTAMP
 
 {%- endmacro %}
