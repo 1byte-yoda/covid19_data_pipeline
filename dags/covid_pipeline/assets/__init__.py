628bef959a0fdc25f09bb5ae78a4ac8315152655
1byte-yoda <dmc.markr@gmail.com>
2025-05-04 20:45:26 +0800

    Added group name for dbt and dlt assets

--- dags/covid_pipeline/assets/__init__.py
+++ dags/covid_pipeline/assets/__init__.py
@@ -1,6 +1,9 @@
+from typing import Optional, Any
+
 from dagster import DailyPartitionsDefinition, AssetKey, AssetSpec, AssetDep
 from dagster_dlt import DagsterDltTranslator
 from dagster_dlt.translator import DltResourceTranslatorData
+from dagster_dbt.dagster_dbt_translator import DagsterDbtTranslator
 
 
 daily_partitions = DailyPartitionsDefinition(start_date="2020-01-22", end_date="2023-03-09", fmt="%Y-%m-%d")
@@ -12,3 +15,11 @@ class CustomDagsterDltTranslator(DagsterDltTranslator):
         default_spec = super().get_asset_spec(data)
         deps = [AssetDep(asset=AssetKey(f"dlt_{data.resource.source_name}"), partition_mapping=d.partition_mapping) for d in default_spec.deps]
         return default_spec.replace_attributes(key=AssetKey(f"{data.resource.name}"), deps=deps)
+
+    def get_group_name(self, dbt_resource_props: dict[str, Any]) -> Optional[str]:
+        return "covid"
+
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_group_name(self, dbt_resource_props: dict[str, Any]) -> Optional[str]:
+        return "covid"
